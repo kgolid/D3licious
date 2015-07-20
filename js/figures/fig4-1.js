1cdeece5c8772b63f9ccb5ec1df614107ced771e
Kjetil Golid <kjetil.golid@gmail.com>
2015-07-20 19:24:34 +0200

    Add line chart with hover selection

--- js/figures/fig4-1.js
+++ js/figures/fig4-1.js
@@ -3,6 +3,7 @@ var d3 = require('d3');
 var svg, legend;
 var w = 900,
     h = 600,
+    padding = 60,
     r = 2;
 
 var xColumn, yColumn, tColumn;
@@ -13,46 +14,82 @@ var setup = function () {
   yColumn = "antall";
   tColumn = "alder";
 
-  xScale = d3.scale.linear().range([0, w]);
-  yScale = d3.scale.linear().range([h, 0]);
+  xScale = d3.scale.linear().range([padding, w-padding]);
+  yScale = d3.scale.linear().range([h - padding, padding]);
   tScale = d3.scale.category20();
 
   svg = d3.select('.fig4-1').append('svg').attr('width', w).attr('height', h);
   legend = d3.select('.fig4-1').append('div');
 
   d3.csv('http://data.ssb.no/api/v0/dataset/65195.csv?lang=no', type, function (data) {
-    draw(data, "");
+    var filtered = data.filter(function (d) { return d['kjønn'] == '0 Begge kjønn'; })
+    displayLegend(filtered);
   });
-}
+};
 
 var type = function (d) {
   d["antall"] = +d["Personer etter alder, kjønn og tid"];
-  d["tid"] = +d["tid"];
+  d["tid"] = d["tid"];
+  d["alder"] = getString(d["alder"]);
   return d;
-}
+};
 
-var draw = function (data, chosen) {
+var draw = function (data) {
+  svg.selectAll(".line").remove();
+
+  var path = svg.append("path").datum(data);
+  path.attr("class", "line")
+      .attr("d", line)
+      .attr("stroke", function (d) {
+        return tScale(d[0][tColumn]);
+      });
+};
+
+var run = function () {
+  setup();
+};
+
+var displayLegend = function (data) {
   xScale.domain( d3.extent(data, function (d) { return d[xColumn]; }) );
   yScale.domain( [0, d3.max(data, function (d) { return d[yColumn]; })] );
 
-  var points = svg.selectAll("circle").data(data);
-  points.enter().append("circle")
-        .attr("cx", function (d){ return xScale(d[xColumn]); })
-        .attr("cy", function (d){ return yScale(d[yColumn]); })
-        .attr("fill", function (d){ return tScale(d[tColumn]); })
-        .attr("r", r)
-  points.attr("opacity", function (d) {
-    return (d[tColumn] == chosen)? 1:0;
-  });
+  var xAxis = d3.svg.axis().scale(xScale).orient('bottom').ticks(10).tickFormat(d3.format("f"));
+  var yAxis = d3.svg.axis().scale(yScale).orient('left').ticks(6);
+
+  svg.append('g').attr('class', 'axis')
+     .attr("transform", "translate(0," + (h - padding) + ")")
+     .call(xAxis);
+
+  svg.append('g').attr('class', 'axis')
+     .attr("transform", "translate(" + padding + ", 0)")
+     .call(yAxis);
 
   legend.selectAll('p').data(getTypes(data)).enter().append('p')
         .html(function (d) { return d; })
         .style('background-color', function (d) { return tScale(d); })
-        .on('click', function (d) { draw(data, d); })
-}
+        .on('mouseover', function (d) {
+          var filteredData = data.filter( function (x) { return x['alder'] == d; });
+          draw(filteredData);
+        });
+};
 
-var run = function () {
-  setup();
+var line = d3.svg.line()
+  .x(function(d) { return xScale(d[xColumn]); })
+  .y(function(d) { return yScale(d[yColumn]); })
+  .interpolate('basis');
+
+var getString = function (string) {
+  return string.substring(string.indexOf(' '));
+};
+
+var categorize = function (data) {
+  var cat = [];
+  data.forEach(function (d) {
+    cat[ d['alder'] ] = cat[ d['alder'] ] || [];
+    cat[ d['alder'] ].push(d);
+  });
+
+  return cat;
 }
 
 var getTypes = function (data) {
@@ -63,8 +100,12 @@ var getTypes = function (data) {
     }
   });
   return categories;
-}
+};
+
+var isOfType = function (d, type) {
+  return d['alder'] == type;
+};
 
 module.exports = {
   run: run
-}
+};
