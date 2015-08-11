a7449d047969357b1a31766916305c183599e663
Kjetil Golid <kjetil.golid@gmail.com>
2015-08-11 15:24:50 +0200

    Animate circle points

--- js/figures/fig7-1.js
+++ js/figures/fig7-1.js
@@ -4,53 +4,69 @@ var w = 900;
 var h = 450;
 var center = [w/2,h/2];
 
-var n = 15;     // Number of points in circle.
+var n = 60;     // Number of points in circle.
 var r = 150;    // Radius of circle.
-var o = 30;     // Max random offset.
+var o = 50;     // Max random offset.
 
 var data = [];
+var time = 0;
 
 var interval;
 
 function addPoints() {
   for (var i = 0; i < n; i++) {
-    var offset = Math.random() * o - o/2;
-    data[i] = offset;
+    data[i] = 0;
   }
 };
+
+function updatePoints() {
+  data = data.map(function () {
+    return Math.random() * o - o/2;
+  })
+}
 
 function displayPoints() {
   var points = svg.selectAll('.point').data(data)
   points.enter().append('circle')
-    .attr('cx', function (d,i) { return getX(i); })
-    .attr('cy', function (d,i) { return getY(i); })
-    .attr('r', 5)
+    .attr('r', 2)
     .attr('class', 'point');
-  points.transition().duration(500).delay(0).ease('elastic')
+  points.transition().duration(400).delay(0).ease('linear')
     .attr('cx', function (d,i) { return getX(i,d); })
     .attr('cy', function (d,i) { return getY(i,d); })
 };
 
+function displayLines() {
+  var lines = svg.selectAll('.line').data(data)
+  lines.enter().append('line')
+    .attr('class', 'line');
+  lines.transition().duration(400).delay(0).ease('linear')
+    .attr('x1', function (d,i) { return getX(i,-50); })
+    .attr('y1', function (d,i) { return getY(i,-50); })
+    .attr('x2', function (d,i) { return getX(i,d); })
+    .attr('y2', function (d,i) { return getY(i,d); })
+};
+
 function getX(i,d) {
-  var dist = d? r + d : r;
-  var phi = (i/n * Math.PI*2);
-  return center[0] + dist * (Math.cos(phi));
+  var phi = (i/n * Math.PI*2) + time/20;
+  return center[0] + (d + r) * (Math.cos(phi));
 };
 
 function getY(i,d) {
-  var dist = d? r + d : r;
-  var phi = (i/n * Math.PI*2);
-  return center[1] + dist * (Math.sin(phi));
+  var phi = (i/n * Math.PI*2) + time/20;
+  return center[1] + (d + r) * (Math.sin(phi));
 };
 
 function setup() {
   svg = d3.select('.fig7-1').append('svg').attr('width', w).attr('height', h);
   addPoints();
   displayPoints();
+  displayLines();
 };
 
 function draw() {
-  addPoints();
+  time++;
+  updatePoints();
+  displayLines();
   displayPoints();
 }
 
@@ -58,12 +74,13 @@ function run() {
   setup();
   interval = setInterval(function () {
     draw();
-  }, 500);
+  }, 400);
 };
 
 function stop() {
   clearInterval(interval);
   data = [];
+  time = 0;
 }
 
 module.exports = {
